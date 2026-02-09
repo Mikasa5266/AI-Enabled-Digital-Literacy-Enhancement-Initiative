477f4bc6e2832317ef6e4d82e971b6b3239ea97b
Mikasa5266 <lyh244140591@whut.edu.cn>
2026-02-10 01:16:11 +0800

    课程创作界面显现

---
+++ frontend/src/types/course_create.ts
@@ -0,0 +1,8 @@
+export interface Course_Create_Info{
+    theme:string
+    targetmember:string
+    timelenth:number
+    chapterlenth:[number,number]
+    inferresourse:string
+    introduction:string
+}
